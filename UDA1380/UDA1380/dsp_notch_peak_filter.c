550119b25a9d43b762a41afb324e805777ff66f9
paulo <paulo>
2018-01-29 20:48:08 +0000

    DSP NOTCHPEAK FILTER

--- UDA1380/UDA1380/dsp_notch_peak_filter.c
+++ UDA1380/UDA1380/dsp_notch_peak_filter.c
@@ -1,12 +1,80 @@
 #include <stdbool.h>
 #include "dsp_notch_peak_filter.h"
+#include "math.h"
 
-bool dsp_notch_peak_filter_init(dsp_t* dsp)
+bool dsp_notch_peak_filter_init(dsp_t* dsp , )
 {
-	
+
+
+
+	int flag;
+	float omega_zero = 0;
+	float delta_omega = 0;
+	float b = 0;
+
+	flag = dsp->function_params->flag;
+
+	/* Calcular o parametro faltando */
+
+	if (dsp->function_params->specification == DSP_NOTCH_PEAK_FILTER_SPECIFICATION_BW_CENTER_FREQ	)   { 
+
+		dsp->function_params->quality_factor = dsp->function_params->center_frequency / dsp->function_params->bandwidht;
+
+
+
+
+	} else if ( dsp->function->params->specification == DSP_NOTCH_PEAK_FILTER_SPECTIFICATION_Q_CENTER_FREQ ) { 
+
+
+		dsp->function_params->bandwidht = dsp->function_params->center_frequency / dsp->function_params->quality_factor;
+
+
+	}
+
+
+	/*  Calcular os parametros do filtro */
+
+
+	omega_zero = (2*3.14*dsp->function_params->center_frequency) / dsp->common_params->sample_rate;
+	delta_omega = (2*3.14*dsp->function_params->bandwidht) / dsp->common_params->sample_rate;
+
+	b = 1 / (1 + tan(delta_omega / 2 ) );
+
+	if (flag == PEAK ) { 
+
+		dsp->function_params->coeffs[0] = 1- b;
+		dsp->function_params->coeffs[1] = 0;
+		dsp->function_params->coeffs[2] = -(1 - b);
+		dsp->function_params->coeffs[3] = -2*b*cos(omega_zero);
+		dsp->function_params->coeffs[4] = -(2*b - 1);
+
+
+			} else if(flag == NOTCH) {
+
+
+
+		dsp->function_params->coeffs[0] = b;
+		dsp->function_params->coeffs[1] = -2*b*cos(omega_zero);
+		dsp->function_params->coeffs[2] = b;
+		dsp->function_params->coeffs[3] = -2*b*cos(omega_zero);
+		dsp->function_params->coeffs[4] = -(2*b - 1);
+
+
+			}
+
+
+	/* Inicalizar Estrutura */
+
+
+		arm_biquad_cascade_df1_initf32(dsp->function_params->S , 1 , dsp->function_params->coeffs , dps->function_params->state);
+
+
 }
 
 bool dsp_notch_peak_filter_step(dsp_t* dsp, const float in[], float out[], void* other)
 {
+
+
+	arm_biquad_cascade_fd1_f32(dsp->function_params->S , in , out , dsp->common_params->block_size);
 	
 }
--- UDA1380/UDA1380/dsp_notch_peak_filter_params.h
+++ UDA1380/UDA1380/dsp_notch_peak_filter_params.h
@@ -4,6 +4,10 @@
 #include "arm_math.h"
 #include "dsp_common.h"
 
+
+#define PEAK 0
+#define NOTCH 1
+
 typedef enum
 {
 	DSP_NOTCH_PEAK_FILTER_SPECIFICATION_BW_CENTER_FREQ,
@@ -11,15 +15,18 @@ typedef enum
 	DSP_NOTCH_PEAK_FILTER_SPECIFICATION_COEFFICIENTS
 } dsp_notch_peak_filter_specification_t;
 
+
+
 typedef struct
 {
 	arm_biquad_casd_df1_inst_f32 S;
 	float coeffs[5];
 	float state[4];
 	dsp_notch_peak_filter_specification_t specification;
-	float bandwidth;
+	float bandwidth; 
 	float center_frequency;
-	float quality_factor;
+	float quality_factor; 
+	int flag;
 } dsp_notch_peak_filter_params_t;
 
 #endif // DSP_NOTCH_PEAK_FILTER_PARAMS_H
